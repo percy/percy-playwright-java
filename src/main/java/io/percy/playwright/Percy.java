66babc56771248331c144a091e2977e9f087808d
prklm10 <pradumraj360@gmail.com>
2024-06-24 10:59:21 +0530

    docs url update

--- src/main/java/io/percy/playwright/Percy.java
+++ src/main/java/io/percy/playwright/Percy.java
@@ -198,7 +198,7 @@ public class Percy {
      */
     public JSONObject snapshot(String name, Map<String, Object> options) {
         if (!isPercyEnabled) { return null; }
-        if ("automate".equals(sessionType)) { throw new RuntimeException("Invalid function call - snapshot(). Please use screenshot() function while using Percy with Automate. For more information on usage of PercyScreenshot, refer https://docs.percy.io/docs/integrate-functional-testing-with-visual-testing"); }
+        if ("automate".equals(sessionType)) { throw new RuntimeException("Invalid function call - snapshot(). Please use screenshot() function while using Percy with Automate. For more information on usage of PercyScreenshot, refer https://www.browserstack.com/docs/percy/integrate/functional-and-visual"); }
 
         Map<String, Object> domSnapshot = null;
         try {
@@ -229,7 +229,7 @@ public class Percy {
      */
     public JSONObject screenshot(String name, Map<String, Object> options) throws Exception {
         if (!isPercyEnabled) { return null; }
-        if ("web".equals(sessionType)) { throw new RuntimeException("Invalid function call - screenshot(). Please use snapshot() function for taking screenshot. screenshot() should be used only while using Percy with Automate. For more information on usage of snapshot(), refer doc for your language https://docs.percy.io/docs/end-to-end-testing"); }
+        if ("web".equals(sessionType)) { throw new RuntimeException("Invalid function call - screenshot(). Please use snapshot() function for taking screenshot. screenshot() should be used only while using Percy with Automate. For more information on usage of snapshot(), refer doc for your language https://www.browserstack.com/docs/percy/integrate/overview"); }
         setPageMetadata();
         JSONObject json = new JSONObject();
         json.put("sessionId", pageMetadata.getSessionId());
@@ -360,7 +360,7 @@ public class Percy {
                 log("You may be using @percy/agent" +
                         "which is no longer supported by this SDK." +
                         "Please uninstall @percy/agent and install @percy/cli instead." +
-                        "https://docs.percy.io/docs/migrating-to-percy-cli"
+                        "https://www.browserstack.com/docs/percy/migration/migrate-to-cli"
                 );
 
                 return false;
